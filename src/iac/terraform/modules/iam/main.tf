84e337a6ecd30e752406c9a4e0a456ff9df51e71
mcouto0xml <murilo.oliveira@sou.inteli.edu.br>
2026-03-23 18:51:48 -0300

    feat: add gke vpc and permission tested and applied

--- src/iac/terraform/modules/iam/main.tf
+++ src/iac/terraform/modules/iam/main.tf
@@ -21,3 +21,34 @@ resource "google_project_iam_member" "bq_looker_reader" {
   role    = "roles/bigquery.dataViewer"
   member  = "serviceAccount:${google_service_account.bq_looker_reader.email}"
 }
+
+resource "google_project_iam_member" "log_writer" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${var.gke_sa_email}"
+}
+
+resource "google_project_iam_member" "metric_writer" {
+  project = var.project_id
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${var.gke_sa_email}"
+}
+
+resource "google_project_iam_member" "artifact_reader" {
+  project = var.project_id
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${var.gke_sa_email}"
+}
+
+resource "google_project_iam_member" "storage_viewer" {
+  project = var.project_id
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${var.gke_sa_email}"
+}
+
+resource "google_project_iam_member" "bigquery_user" {
+  project = var.project_id
+  role    = "roles/bigquery.dataViewer"
+  member  = "serviceAccount:${var.gke_sa_email}"
+}
+
