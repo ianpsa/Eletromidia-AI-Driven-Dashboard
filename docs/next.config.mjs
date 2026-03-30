c008a2f44ef3a14a2d742c72adc7aa998cca5336
Davi Abreu da Silveira <daviabreudasilveira@gmail.com>
2026-03-29 22:27:30 -0300

    fixes

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,14 +1,13 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
-const isProduction = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'export',
   reactStrictMode: true,
-  basePath: isProduction ? '/2026-1a/t12/g05' : '',
-  assetPrefix: isProduction ? '/2026-1a/t12/g05/' : undefined,
+  basePath: '/2026-1a/t12/g05',
+  assetPrefix: '/2026-1a/t12/g05/',
   images: {
     unoptimized: true,
   },
