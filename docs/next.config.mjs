280fe9520f092ba1076628ddaf12e6a20f87fe86
yasmimpassos <yasmim.passos@sou.inteli.edu.br>
2026-03-29 22:02:48 -0300

    docs(hexbin): adiciona a docs do dashboard

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,13 +1,14 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
+const isProduction = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'export',
   reactStrictMode: true,
-  basePath: '/2026-1a/t12/g05',
-  assetPrefix: '/2026-1a/t12/g05/',
+  basePath: isProduction ? '/2026-1a/t12/g05' : '',
+  assetPrefix: isProduction ? '/2026-1a/t12/g05/' : undefined,
   images: {
     unoptimized: true,
   },
