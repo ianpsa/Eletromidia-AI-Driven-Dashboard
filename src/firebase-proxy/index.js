4cbd6c4f932aa24b5176a39abc1c57e408ee6ef8
mcouto0xml <murilo.oliveira@sou.inteli.edu.br>
2026-04-01 17:53:59 -0300

    feat: add ingress nginx tested and applied

--- src/firebase-proxy/index.js
+++ src/firebase-proxy/index.js
@@ -8,6 +8,15 @@ app.use(
   createProxyMiddleware({
     target: "http://34.39.196.243",
     changeOrigin: true,
+    on: {
+      proxyReq: (proxyReq, req) => {
+        // Repassa o Authorization explicitamente
+        const auth = req.headers["authorization"];
+        if (auth) {
+          proxyReq.setHeader("Authorization", auth);
+        }
+      },
+    },
   })
 );
 
